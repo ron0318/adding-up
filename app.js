32745f3e66c5670d22d4a01458ba296e5941d189
ron0318 <ron0318@ezweb.ne.jp>
2020-03-15 23:48:02 +0900

    人が 減った割合 のランキングの実装

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 'use strict';
 const fs = require('fs');
-const readline= require('readline');
+const readline = require('readline');
 const rs = fs.createReadStream('./popu-pref.csv');
-const rl = readline.createInterface({'input': rs, 'output':{}});
+const rl = readline.createInterface({ 'input': rs, 'output': {} });
 const prefectureDateMap = new Map(); // key:都道府県 value:集計データのオブジェクト
-rl.on ('line', (lineString) => {
+rl.on('line', (lineString) => {
     const columns = lineString.split(',');
     const year = parseInt(columns[0]);
     const prefecture = columns[1];
     const popu = parseInt(columns[3]);
-    if (year === 2010 || year ===2015) {
+    if (year === 2010 || year === 2015) {
         let value = prefectureDateMap.get(prefecture);
         if (!value) {
             value = {
@@ -32,11 +32,12 @@ rl.on('close', () => {
         value.change = value.popu15 / value.popu10;
     }
     const rankingArray = Array.from(prefectureDateMap).sort((pair1, pair2) => {
-        return pair2[1].change - pair1[1].change;
+        return pair1[1].change - pair2[1].change;
+    })
+    const rankingStrings = rankingArray.map(([key, value], i=1) => {
+        return (i + 1) + '位 ' + key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率' + value.change;
     });
-    console.log(rankingArray);
-    const rankingString = rankingArray.map(([key, value]) => {
-        return key + ': ' + value.popu10 + '=>' + value.popu15 + '   変化率:' + value.change; 
-    });
-    console.log(rankingString);
+    console.log(rankingStrings);
 });
+
+
